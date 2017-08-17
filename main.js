e121bf0d90f73294b2fe419eca1d41ebab743562
Arden Diakhate-Palme <ardendiak@gmail.com>
2017-08-17 10:27:53 +0800

    test newspage complete with style in css

--- main.js
+++ main.js
@@ -49,7 +49,38 @@ $(document).ready(function(){
         adaptiveHeight: true,
         arrows: false,
         speed: 500,
-        dots: true,
+        dots: false,
+    });
+    $('.center_slideshow').slick({
+		  centerMode: true,
+		  autoplay: true,
+	        autoplaySpeed : 3000,
+	        adaptiveHeight: true,
+	        arrows: false,
+	        speed: 500,
+	        dots: false,
+		  centerPadding: '60px',
+		  slidesToShow: 3,
+		  responsive: [
+		    {
+		      breakpoint: 768,
+		      settings: {
+		        arrows: false,
+		        centerMode: true,
+		        centerPadding: '40px',
+		        slidesToShow: 3
+		      }
+		    },
+		    {
+		      breakpoint: 480,
+		      settings: {
+		        arrows: false,
+		        centerMode: true,
+		        centerPadding: '40px',
+		        slidesToShow: 1
+		      }
+		    }
+		  ]
     });
 
 	var title = $('title').text();
